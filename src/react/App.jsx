6b5b79394730108a0e6a8017dba247fa891caf5d
Dennis Axelsson <dennis.axelsson@etraveli.com>
2016-02-25 17:34:03 +0100

    Episode 26: Passing the Store Down with <Provider> from React Redux.

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -2,22 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoApp from '../redux/TodoApp';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import AppReducer from '../redux/AppReducer';
 
 const store = createStore(AppReducer, undefined, window.devToolsExtension ? window.devToolsExtension() : undefined);
-
-const Provider = React.createClass({
-
-    getChildContext(){
-        return {
-            store: this.props.store
-        };
-    },
-
-    render(){
-        return this.props.children;
-    }
-});
 
 Provider.childContextTypes = {
     store: React.PropTypes.object
