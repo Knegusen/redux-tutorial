6e9fdf1a1517d2dc66965a6446c9a675695d0369
Dennis Axelsson <dennis.axelsson@etraveli.com>
2016-03-07 09:50:35 +0100

    Add specs for Actions.

---
+++ src-test/redux/ActionsSpec.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+    addTodo, toggleTodo, setVisibilityFilter,
+    ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER
+} from '../../src/redux/Actions';
+
+describe('Actions', () => {
+    describe('add todo', () => {
+        it('returns correct type', () => {
+            expect(addTodo().type).toBe(ADD_TODO);
+        });
+
+        it('returns correct text', () => {
+            const text = 'text';
+            expect(addTodo(text).text).toBe(text);
+        });
+
+        it('increment id', () => {
+            const id = addTodo().id;
+            expect(addTodo().id).toBe(id + 1);
+        });
+    });
+
+    describe('toggle todo', () => {
+        it('returns correct type', () => {
+            expect(toggleTodo().type).toBe(TOGGLE_TODO);
+        });
+
+        it('returns correct id', () => {
+            const id = 'id';
+            expect(toggleTodo(id).id).toBe(id);
+        });
+    });
+
+    describe('set visibility filter', () => {
+        it('returns correct type', () => {
+            expect(setVisibilityFilter().type).toBe(SET_VISIBILITY_FILTER);
+        });
+        it('returns correct type', () => {
+            const filter = 'filter';
+            expect(setVisibilityFilter(filter).filter).toBe(filter);
+        });
+    });
+});
--- src/redux/Actions.js
+++ src/redux/Actions.js
@@ -1,6 +1,6 @@
-const ADD_TODO = 'ADD_TODO';
-const TOGGLE_TODO = 'TOGGLE_TODO';
-const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
 
 export const VisibilityFilters = {
     SHOW_ALL: 'SHOW_ALL',
